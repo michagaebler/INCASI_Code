b6a24e0681e24d4286e9d59bde0925eb4497e747
montagnero <michael.gaebler@gmail.com>
2016-10-13 08:37:37 +0200

    initial analyses

--- INCASI_read_dat.R
+++ INCASI_read_dat.R
@@ -1,5 +1,5 @@
 require(R.matlab)
-#require(RHRV)
+
 
 # set working directory
 setwd('D:/INCASI/Labtest/20171007') 
@@ -44,4 +44,50 @@ ecgduration_s <- (ecg_events[2,3] - ecg_events[1,3]) / ecg_sf
 
 # deviation between ECG and logfile length between markers (in s)
 deviation_s <- abs(ecgduration_s - logduration_s)
+
+#### exploration
+
+log$pic_duration <- log$picoffset - log$piconset
+
+#### peak detection with Kubios
+
+
+exg1_crop <- ecg$EXG1[ecg_events[1,3]:length(ecg$EXG1)]
+exg2_crop <- ecg$EXG2[ecg_events[1,3]:length(ecg$EXG2)]
+
+#exg1_crop_resamp <- resample(exg1_crop, 1000, 2048) # get resample() from signal
+
+write.csv(exg1_crop, 'EXG1_cropped_4Kubios.csv')
+write.csv(exg2_crop, 'EXG2_cropped_4Kubios.csv')
+
+exg1_peaks <- read.csv('EXG1_cropped_peaks_from_Kubios.txt')
+exg2_peaks <- read.csv('EXG2_cropped_peaks_from_Kubios.txt')
+
+exg1_peaks <- as.vector(exg1_peaks[,1])
+exg2_peaks <- as.vector(exg2_peaks[,1])
+
+
+#### analyse encoding
+
+diff_ecg_log <- ecgonset_s 
+log$clickonset_ecgtime_s <- (log$clickonset/1000 - logonset_s) 
+log$clickonset_ecgtime_dpt <- log$clickonset_ecgtime_s * ecg_sf
+
+log_encode <- subset(log, log$block == 1)
+
+log_encode$clickonset_ecgtime_s
+
+for (ipt in 1:length(log_encode$clickonset_ecgtime_s)) {
+  
+
+position <-  min(which(exg1_peaks > log_encode$clickonset_ecgtime_s[ipt]))
+  
+#  log_encode$clickonset_ecgtime_s[ipt] - exg1_peaks[position]
+#  log_encode$clickonset_ecgtime_s[ipt] - exg1_peaks[position+1]
+  
+  
+  
+}
+
+
 
